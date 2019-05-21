4189482f6ec6e31e188c8d6d81ff1f9f5bc7bb9f
Cole Hudson <cole@colejhudson.com>
2019-05-21 09:46:51 -0700

    adapted for use with AWS lambda

--- api.py
+++ api.py
@@ -1,58 +1,61 @@
-from flask import Flask
-import tweepy
 from ibm_watson import PersonalityInsightsV3
+import tweepy
 import json
 import time
+import os
 
-app = Flask(__name__)
+IBM_API = 'https://gateway.watsonplatform.net/personality-insights/api'
 
-# Twitter and PI credentials go here
+TWITTER_CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+TWITTER_ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+TWITTER_ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
 
-TWITTER_AUTH = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY,TWITTER_CONSUMER_SECRET)
-TWITTER_AUTH.set_access_token(TWITTER_ACCESS_TOKEN,TWITTER_ACCESS_TOKEN_SECRET)
+IBM_API_USERNAME = os.environ['IBM_API_USERNAME']
+IBM_API_PASSWORD = os.environ['IBM_API_PASSWORD']
+
+TWITTER_AUTH = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+TWITTER_AUTH.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
 TWITTER = tweepy.API(TWITTER_AUTH)
 
-
-twitter_user = TWITTER.user_timeline(screen_name='austen',
-                                     count=100,
-                                     tweet_mode='extended')
-
-favorites = TWITTER.favorites('austen',count=100)
-
-# t = []
-# for i in twitter_user: t.append(i.created_at)
-
 def convert_status_to_pi_content_item(t,f):
-            return {
-                'content': t.full_text + f.text,
-                'contenttype': 'text/plain',
-                'created': int(time.mktime(t.created_at.timetuple())),
-                'id': str(t.id),
-                'language': t.lang
-            }
+    return {
+        'content': t.full_text + f.text,
+        'contenttype': 'text/plain',
+        'created': int(time.mktime(t.created_at.timetuple())),
+        'id': str(t.id),
+        'language': t.lang
+    }
 
-pi_content_items_array = list(map(convert_status_to_pi_content_item, twitter_user,
-                                  favorites))
+def api(event, context):
+    twitter_user = TWITTER.user_timeline(
+        screen_name='austen',
+        count=100,
+        tweet_mode='extended'
+    )
 
-pi_content_items = {'contentItems': pi_content_items_array}
+    favorites = TWITTER.favorites('austen',count=100)
 
-data = json.dumps(pi_content_items, indent=2)
-
-personality_insights = PersonalityInsightsV3(
-    version='2017-10-13',
-    url=pi_url,
-    iam_apikey= pi_password)
-
-profile = personality_insights.profile(
-          data,
-          accept='application/json',
-          content_type='application/json',
-          consumption_preferences=True,
-          raw_scores=True).get_result()
-
-# Returning profile without json.dump might be a better option
-# profile = json.dumps(profile, indent=2)
-
-@app.route('/')
-def hello_world():
-    return profile
+    personality_insights = PersonalityInsightsV3(
+        version='2017-10-13',
+        url=IBM_API,
+        iam_apikey=IBM_API_PASSWORD
+    )
+    
+    pi_content_items_array = list(map(convert_status_to_pi_content_item, twitter_user, favorites))
+    pi_content_items = {'contentItems': pi_content_items_array}
+    data = json.dumps(pi_content_items, indent=2)
+    
+    profile = personality_insights.profile(
+        data,
+        accept='application/json',
+        content_type='application/json',
+        consumption_preferences=True,
+        raw_scores=True
+    ).get_result()
+   
+    return {
+        'statusCode': 200,
+        'body': profile
+    }
