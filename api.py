5bd53bed98e670d05de5c3553934578a22551737
Cole Hudson <cole@colejhudson.com>
2019-05-21 11:11:35 -0700

    refactored and added support for POST requests

--- api.py
+++ api.py
@@ -1,8 +1,5 @@
-from ibm_watson import PersonalityInsightsV3
-import tweepy
-import json
-import time
-import os
+import tweepy, ibm_watson
+import json, time, os
 
 IBM_API = 'https://gateway.watsonplatform.net/personality-insights/api'
 
@@ -15,11 +12,13 @@ IBM_API_USERNAME = os.environ['IBM_API_USERNAME']
 IBM_API_PASSWORD = os.environ['IBM_API_PASSWORD']
 
 TWITTER_AUTH = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-TWITTER_AUTH.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+TWITTER_AUTH.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
 
 TWITTER = tweepy.API(TWITTER_AUTH)
 
-def convert_status_to_pi_content_item(t,f):
+DEFAULT_USERNAME = 'austen'
+
+def format_for_ibm_api(t,f):
     return {
         'content': t.full_text + f.text,
         'contenttype': 'text/plain',
@@ -28,34 +27,28 @@ def convert_status_to_pi_content_item(t,f):
         'language': t.lang
     }
 
-def main(event, context):
-    twitter_user = TWITTER.user_timeline(
-        screen_name='austen',
-        count=100,
-        tweet_mode='extended'
-    )
+def twitter_to_personality_scores(timeline, favorites):
+    insights = ibm_watson.PersonalityInsightsV3(version='2017-10-13', url=IBM_API, iam_apikey=IBM_API_PASSWORD)
 
-    favorites = TWITTER.favorites('austen',count=100)
+    ibm_formatted_tweets = list(map(format_for_ibm_api, timeline, favorites))
+    tweets = json.dumps({ 'contentItems': pi_content_items_array }, indent=2)
 
-    personality_insights = PersonalityInsightsV3(
-        version='2017-10-13',
-        url=IBM_API,
-        iam_apikey=IBM_API_PASSWORD
-    )
-    
-    pi_content_items_array = list(map(convert_status_to_pi_content_item, twitter_user, favorites))
-    pi_content_items = {'contentItems': pi_content_items_array}
-    data = json.dumps(pi_content_items, indent=2)
-    
-    profile = personality_insights.profile(
-        data,
+    scores = insights.profile(
+        tweets,
         accept='application/json',
         content_type='application/json',
         consumption_preferences=True,
         raw_scores=True
-    ).get_result()
-   
-    return {
-        'statusCode': 200,
-        'body': json.dumps(profile)
-    }
+    )
+
+    return scores.get_result()
+
+def main(event, context):
+    username = json.loads(event['body'] or {}).get('username', DEFAULT_USERNAME)
+
+    timeline = TWITTER.user_timeline(screen_name=username, count=100, tweet_mode='extended')
+    favorites = TWITTER.favorites(username, count=100)
+
+    body = json.dumps(twitter_to_personality_score(timeline, favorites))
+
+    return { 'statusCode': 200, 'body': body }
